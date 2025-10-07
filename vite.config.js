f0f1b003a44dfe14516fbae84c82e027ee930f6c
twassim1234 <93616362+twassim1234@users.noreply.github.com>
2025-10-07 17:25:40 +0100

    main

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/wassim-trabelsi-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
 });
