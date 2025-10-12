089d9d2779748cd547813ad863c131dc3a67efd3
twassim1234 <93616362+twassim1234@users.noreply.github.com>
2025-10-12 23:06:43 +0100

    updated CV

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -109,7 +109,7 @@ export const CertificationsSection = () => {
           Here are some of the certifications I have earned to enhance my knowledge and skills in technology and project management.
         </p>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {certifications.map((cert, key) => (
             <div
               key={key}
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -27,7 +27,7 @@ export const ContactSection = () => {
 
     emailjs
     .sendForm(
-      "service_ykodjdr",
+      "service_ouzpvbc",
       "template_otywphn",
       form,
       "OKl83bhVxc81Ojkfu"
@@ -80,7 +80,7 @@ export const ContactSection = () => {
                     href="mailto:hello@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    Wassim.trabelsi.2002@gmail.com
+                    wassim.trabelsi.pro@gmail.com
                   </a>
                   
                 </div>
