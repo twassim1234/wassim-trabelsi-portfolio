af1512af3595af6f4fb6b6612529da1fbc858360
twassim1234 <93616362+twassim1234@users.noreply.github.com>
2025-10-12 23:28:34 +0100

    v1

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public\projects\Wassim_Trabelsi_resume-25.26.pdf"
+                href="src\assets\Wassim_Trabelsi_resume-25.26.pdf"
                 download="Wassim_Trabelsi_resume-25.26.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
